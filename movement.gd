212d53e010223e0b51b8647781ddad88f1b104e9
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2024-12-23 12:34:19 -0500

    merge conflict testing
    merge

--- movement.gd
+++ movement.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 @export var speed = 400
-
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
