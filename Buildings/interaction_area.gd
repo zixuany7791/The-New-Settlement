c586d1c8f89dd498ce0a031df6455583ef6da188
zixuany7791 <zixuany2@nycstudents.net>
2025-01-21 18:08:05 -0500

    fixing attewmpt #1

--- Buildings/interaction_area.gd
+++ Buildings/interaction_area.gd
@@ -1,11 +1,5 @@
-extends Area2D
+class_name interactable extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var interact_label = "none"
+@export var interact_type = "none"
+@export var interact_value = "none"
--- build.gd
+++ build.gd
@@ -1,9 +1,9 @@
 extends Node2D  
 
-var mynode = preload("res://building_block.tscn")
+var mynode = preload("res://buildings/building_block.tscn")
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("Left-Click"):
+	if Input.is_action_just_pressed("LMC"):
 		# Get the current global mouse position dynamically
 		var mouse_position = get_global_mouse_position()
 		inst(mouse_position)
