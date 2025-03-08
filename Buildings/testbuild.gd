eb239d768c76ec0bff2a83da464af5bbb8025494
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-03-07 20:28:36 -0500

    add menu(still may not work yet but is visible)

---
+++ Buildings/testbuild.gd
@@ -0,0 +1,16 @@
+extends Node2D  
+
+var mynode = preload("res://buildings/building_block.tscn")
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("LMC"):
+		# Get the current global mouse position dynamically
+		var mouse_position = get_global_mouse_position()
+		inst(mouse_position)
+		print("Mouse Position: ", mouse_position)
+
+
+func inst(pos):
+	var instance = mynode.instantiate()
+	instance.position = pos
+	add_child(instance)
--- toggling_panel.gd
+++
@@ -1,5 +0,0 @@
-extends Panel
-
-func _input(event):
-	if event.is_action_pressed("toggle_menu"):
-		visible = !visible
