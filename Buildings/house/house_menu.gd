6abcfd59df6a9352858e6b9027dc02471690e35d
zixuany7791 <zixuany2@nycstudents.net>
2025-05-12 01:24:43 -0400

    Added more close button for menu

--- Buildings/house/house_menu.gd
+++ Buildings/house/house_menu.gd
@@ -3,14 +3,21 @@ extends Control
 @onready var delete_button = $delete
 @onready var interact_label = $"../../../Node2D/TileMapLayer/trunk/CharacterBody2D/Interaction Component/InteractLabel"
 @onready var player = $"../../../Node2D/TileMapLayer/trunk/CharacterBody2D"
+@onready var close_button = $close
 func _ready():
 	hide()
 	delete_button.pressed.connect(delete_pressed)
-
+	close_button.pressed.connect(close_pressed)
+	
 func disable_player_movement():
 	player.set_interacting_state(true)
 func enable_player_movement():
 	player.set_interacting_state(false)
+func close_pressed():
+	interact_label.show()
+	enable_player_movement()
+
+	hide()
 func delete_pressed():
 	ResourceManager.resources["capacity"] -= 5
 	if get_node("../../../Node2D/TileMapLayer/Popupmenu").placed_obstacles.has(get_node("../../../Node2D/TileMapLayer/Popupmenu").get_building_position(get_node("../..").position)):
--- panel.gd
+++ panel.gd
@@ -3,10 +3,11 @@ extends Panel
 @onready var sell_button = $Sell
 @onready var interact_label = $"../../../TileMapLayer/trunk/CharacterBody2D/Interaction Component/InteractLabel"
 @onready var player = $"../../../TileMapLayer/trunk/CharacterBody2D"
-
+@onready var close_button = $"close"
 func _ready():
 	hide()
 	sell_button.pressed.connect(sell_wood)
+	close_button.pressed.connect(close_pressed)
 
 func disable_player_movement():
 	player.set_interacting_state(true)
@@ -18,3 +19,8 @@ func sell_wood():
 		ResourceManager.resources["wood"] -= 50
 	else:
 		print("yes")
+func close_pressed():
+	interact_label.show()
+	enable_player_movement()
+
+	hide()
