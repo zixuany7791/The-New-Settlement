85f4795d9ebc2db8f40a1c2cbce74cf7d02f2d43
zixuany7791 <zixuany2@nycstudents.net>
2025-05-27 01:43:25 -0400

    another bug fix...

--- Buildings/house/house.gd
+++ Buildings/house/house.gd
@@ -3,7 +3,8 @@ extends StaticBody2D
 @onready var interaction_area: Area2D = $House_interact
 @onready var sprite_2d: Sprite2D = $Sprite2D
 #
-
+func assign_position(pos):
+	pass
 #
 ## When the player interact with the building, it would print in the console
 #func _on_interact():
