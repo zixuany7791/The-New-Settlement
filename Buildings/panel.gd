fc4973380ef97aa4fd69063b74b5055634167a12
williamz2198 <115739712+williamz2198@users.noreply.github.com>
2025-05-26 20:26:12 -0400

    people btn and add farm

--- Buildings/panel.gd
+++ Buildings/panel.gd
@@ -1,14 +1,16 @@
 extends Panel
 
-@onready var sell_button = $Sell
+@onready var sellWood_button = $SellWood
+@onready var buyPeople_button = $BuyPeople
 @onready var interact_label = $"../../../TileMapLayer/trunk/CharacterBody2D/Interaction Component/InteractLabel"
 @onready var player = $"../../../TileMapLayer/trunk/CharacterBody2D"
 @onready var close_button = $"close"
 @onready var error_label = $"error"
 func _ready():
 	hide()
-	sell_button.pressed.connect(sell_wood)
+	sellWood_button.pressed.connect(sell_wood)
 	close_button.pressed.connect(close_pressed)
+	
 
 func disable_player_movement():
 	player.set_interacting_state(true)
@@ -21,6 +23,12 @@ func sell_wood():
 		error_label.text = ""
 	else:
 		error_label.text = "Not enough wood"
+func buy_people():
+	if ResourceManager.resources["currency"] >= 100:
+		ResourceManager.resources["population"] += 1
+		ResourceManager.resources["currency"] -= 100
+	else:
+		error_label.text = "Not enough money"
 func close_pressed():
 	interact_label.show()
 	enable_player_movement()
